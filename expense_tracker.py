1f54185e29d6916fee0308d4f7b8938a96194b62
Esssencew <cfhtgfg@gmail.com>
2025-07-27 17:08:09 +0300

    new functions
    added some improvmentes

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,48 +1,67 @@
 import csv
+import os
+from datetime import date
 
 FILENAME = "expenses.csv"
+HEADERS = ['date', 'category', 'sum($)', 'description']
 
-def valid_num(n):
-    if n.isdigit():
-        return True
-    else:
-        return False
+# checking the existence of file
+def file_existence():
+    if not os.path.exists(FILENAME):
+        with open(FILENAME, mode='w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(HEADERS)
 
+# adding expense
 def add_expense():
-    date = input('Укажите дату: ')
-    category = input('Укажите категорию: ')
-    summ = float(input('Укажите сумму: '))
-    desc = input('Укажите описание: ')
+    date_input = input('Set the date(YY-MM-DD): ')
+    date_total = date_input if date_input else date.today()
+    category = input('Set the category: ')
+    try:
+        summ = float(input('Enter the amount: '))
+    except ValueError:
+        print('Put the number.')
+        return 
+    desc = input('Enter the description: ')
     with open(FILENAME, mode="a", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow([date, category, summ, desc])
-    print('Расход добавлен!')
+        writer.writerow([date_total, category, summ, desc])
+    print('Expense was added!')
 
+# showing expenses
 def show_expenses():
     print(open('expenses.csv').read())
 
+# counting the sum of expenses
 def expenses_sum():
     total = 0
     with open('expenses.csv', newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         for row in reader:
                 total += float(row['sum($)'])
-    print(f'Сумма ваших расходов: {total}$')
+    print(f'Sum of your amounts: {total}$')
 
-while True:
-    print('1. Добавить расход')
-    print('2. Показать все расходы')
-    print('3. Показать сумму расходов')
-    print('4. Выйти')
-    choice = input('Выберите опцию: ')
+# the main proccess 
+def main():
+    file_existence()
+    while True:
+        print('1. Add expense')
+        print('2. Show all expenses')
+        print('3. Show the sum of your expenses')
+        print('4. Leave')
+        choice = input('Choose the option: ')
 
-    if choice == '4':
-        break
-    else:
-        if choice == '1':
-            add_expense()
-        if choice == '2':
-            show_expenses()
-        if choice == '3':
-            expenses_sum()
-            
+        if choice == '4':
+            break
+        else:
+            if choice == '1':
+                add_expense()
+            if choice == '2':
+                show_expenses()
+            if choice == '3':
+                expenses_sum()
+            else:
+                print('Put the number')
+                continue
+
+main()
